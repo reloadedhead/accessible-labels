d9e956b102ffb518db377ae03e21c1808ee2c2b8
Tomás García Gobet <tomasgobet@icloud.com>
2022-11-14 22:48:07 +0100

    adds html only examples

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,13 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Accessible labels and where to find them</h1>
+
+      <span>1500.50 Kg CO₂</span>
+
+      <div>
+        <span>1500.50</span>{" "}
+        <abbr title="Kilograms of carbon dioxide">Kg CO₂</abbr>
+      </div>
     </div>
   );
 }
